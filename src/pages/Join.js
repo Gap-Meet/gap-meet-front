b11e6a8c4350231642359d94d69436e508d2c463
dlekdms2021 <117347360+dlekdms2021@users.noreply.github.com>
2023-10-04 23:33:14 +0900

    로그인 회원가입 서버연결 중

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,12 +1,44 @@
-import { useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import JoinFinish1 from "../components/JoinFinish1";
 import PortalPopup from "../components/PortalPopup";
 import { useNavigate } from "react-router-dom";
 import styles from "./Join.module.css";
 
+import axios from "axios";
+
 const Join = () => {
+
+  const [userid, setUserid] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(null);
+
   const [isJoinFinishOpen, setJoinFinishOpen] = useState(false);
   const navigate = useNavigate();
+
+  const handleJoin = async () => {
+    try {
+      const response = await axios.post('/signup', {  // 서버의 회원가입 endpoint로 POST 요청
+        userid: userid,
+        password: password,
+        email: email,
+        username: username,
+      });
+
+      if (response.data) {  // 응답 데이터가 있다면 회원가입이 성공한 것으로 간주
+        console.log('회원가입 성공:', response.data);
+        setJoinFinishOpen(true);  // 회원가입 성공 팝업 표시
+      }
+    } catch (err) {  // 오류 발생 시
+      console.error('회원가입 실패:', err);
+      if (err.response && err.response.data && err.response.data.error) {
+        setError(err.response.data.error);  // 서버에서 전송된 에러 메시지 설정
+      } else {
+        setError('회원가입 중 오류 발생');  // 일반 에러 메시지 설정
+      }
+    }
+  };
 
   const openJoinFinish = useCallback(() => {
     setJoinFinishOpen(true);
@@ -24,22 +56,51 @@ const Join = () => {
     <>
       <div className={styles.join}>
         <div className={styles.div}>회원가입</div>
-        <div className={styles.id}>ID</div>
-        <div className={styles.pw}>PW</div>
+        <div className={styles.id}>ID
+          <input 
+            type="text"
+            className="input_id"
+            value={userid}  // 상태값과 연결
+            onChange={(e) => setUserid(e.target.value)}  // 변경사항을 상태에 반영
+          />
+        </div>
+        <div className={styles.pw}>PW
+          <input 
+            type="password" 
+            className="input_pw"
+            value={password}  // 상태값과 연결 
+            onChange={(e) => setPassword(e.target.value)}  // 변경사항을 상태에 반영
+          />
+        </div>
         <div className={styles.div1}>
           사용할 비밀번호를 입력해주세요(숫자 6자리)
         </div>
+        
         <div className={styles.div2}>비밀번호를 다시 입력해주세요</div>
         <div className={styles.div3}>사용할 아이디를 입력해주세요</div>
-        <div className={styles.div4}>이름</div>
+        <div className={styles.div4}>이름
+          <input 
+            type="name" 
+            className="input_name"
+            value={username}  // 상태값과 연결
+            onChange={(e) => setUsername(e.target.value)}  // 변경사항을 상태에 반영
+          />
+        </div>
         <div className={styles.div5}>성함을 입력해주세요</div>
-        <div className={styles.div6}>이메일</div>
+        <div className={styles.div6}>이메일
+          <input 
+            type="email" 
+            className="input_email"
+            value={email}  // 상태값과 연결
+            onChange={(e) => setEmail(e.target.value)}  // 변경사항을 상태에 반영
+          />
+        </div>
         <div className={styles.div7}>이메일을 입력해주세요(중복불가)</div>
         <img
           className={styles.yesButtonIcon}
           alt=""
           src="/yes-button.svg"
-          onClick={openJoinFinish}
+          onClick={handleJoin} // {openJoinFinish}
         />
         <img className={styles.yesButtonIcon1} alt="" src="/yes-button.svg" />
         <img className={styles.yesButtonIcon2} alt="" src="/yes-button.svg" />
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,8 @@
-import { useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./LoginPage.module.css";
+
+import axios from 'axios';
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -9,10 +11,50 @@ const LoginPage = () => {
     navigate("/join");
   }, [navigate]);
 
-  const onYesButtonClick = useCallback(() => {
-    navigate("/schedule");
-  }, [navigate]);
+  
+  const [userid, setUserid] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
+  const handleLogin = async (e) => {
+    e && e.preventDefault(); // e가 존재할 경우에만 preventDefault를 실행
+    setError(null); // 에러 초기화
+
+    try {
+      // 로그인 요청을 보냅니다.
+      // 일단은 로컬서버로 요청
+      const response = await axios.post('/login', {
+        userid: userid, // 이메일을 userid로 사용하거나 필요에 따라 수정
+        password: password,
+      });
+
+      // 토큰을 로컬 스토리지에 저장합니다.
+      if (response.data.token) {
+        localStorage.setItem('userToken', response.data.token);
+        console.log('로그인 성공!');
+        return true; // 로그인 성공 여부를 반환
+        
+        // 로그인 성공 후, 다음 페이지로 이동하거나 원하는 작업을 수행할 수 있습니다.
+        // 예: navigate("/dashboard");
+      }
+    } catch (err) {
+      // 로그인 실패
+      if (err.response && err.response.data && err.response.data.error) {
+        setError(err.response.data.error);
+      } else {
+        setError('로그인 중 오류 발생');
+      }
+    }
+    return false;
+  };
+
+  const onYesButtonClick = useCallback(async () => {
+    const loginSuccess = await handleLogin(); // 로그인 요청 실행
+    if (loginSuccess) {
+        navigate("/schedule"); // 로그인이 성공적으로 완료되면 schedule 페이지로 이동
+    }
+  }, [navigate, handleLogin]);
+  
   return (
     <div className={styles.loginPage}>
       <div className={styles.div}>로그인</div>
@@ -21,8 +63,22 @@ const LoginPage = () => {
       </div>
       <div className={styles.idBlank1} />
       <div className={styles.pwBlank1} />
-      <div className={styles.id}>ID</div>
-      <div className={styles.pw}>PW</div>
+      <div className={styles.id}>ID
+        <input 
+          type="userid"
+          className="input_id"
+          value={userid}  // 상태값과 연결
+          onChange={(e) => setUserid(e.target.value)}  // 변경사항을 상태에 반영
+        />
+      </div>
+      <div className={styles.pw}>PW
+        <input 
+          type="password" 
+          className="input_pw"
+          value={password}  // 상태값과 연결
+          onChange={(e) => setPassword(e.target.value)}  // 변경사항을 상태에 반영
+        />
+      </div>
       <div className={styles.div2}>아이디가 없으신가요?</div>
       <img
         className={styles.yesButtonIcon}
