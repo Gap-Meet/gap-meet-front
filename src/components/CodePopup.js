205acbb3c4876f5a1578d6853b18386583e54f4e
yunss719 <jungha0263@gmail.com>
2023-11-09 19:54:08 +0900

    modified css (input, select)

---
+++ src/components/CodePopup.js
@@ -0,0 +1,18 @@
+import styles from "./CodePopup.module.css";
+
+const CodePopup = () => {
+  return (
+    <div className={styles.codePopup}>
+      <div className={styles.div1}>모임이름과 코드를 입력해주세요!</div>
+      <div className={styles.div2}>모임이름</div>
+      <div className={styles.div3}>비밀코드</div>
+      <div className={styles.urlBlank}>
+        <div className={styles.groupBlank1} />
+        <div className={styles.groupBlank2} />
+      </div>
+      <img className={styles.yesButton2Icon} alt="" src="/yes-button21.svg" />
+    </div>
+  );
+};
+
+export default CodePopup;
---
+++ src/components/ID_X.js
@@ -0,0 +1,11 @@
+import styles from "./ID_X.module.css";
+
+const ID_X = () => {
+  return (
+    <div className={styles.ID_X}>
+      <div className={styles.div}>이미 존재하는 아이디입니다.</div>
+    </div>
+  );
+};
+
+export default ID_X;
